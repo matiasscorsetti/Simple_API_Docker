c1899e8a64da0d3113b954c6e2ac43de4be5e675
matiasscorsetti <47259070+matiasscorsetti@users.noreply.github.com>
2020-12-30 17:48:20 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ Steps:
 - go to the downloaded directory and run:
   - ```docker-compose build```
   - ```docker-compose up```
-- go to http://localhost:1001/docs
+- go to:
+	- http://localhost:1000/docs
+	- http://localhost:1001/docs
+	- http://localhost:1002/docs
--- app/main.py
+++ app/main.py
@@ -4,44 +4,57 @@ from random import choice
 from json import load
 from os import environ
 
-class ProcessConfig(BaseModel):
-    file_name: str
-
-dir_config = "dir_config/"
+class ProcessFile(BaseModel):
+    '''
+    the file to process
+    '''
+    file_name: str # name of the file to process
 
 app = FastAPI()
+
+# access environment variable values
+site_name = environ['SITENAME']
+
+# read configuration file
+try:
+    with open("config_file.json", 'r', encoding='latin-1') as f:
+        config = load(f)
+
+except FileNotFoundError:
+    config = "configuration file not found"
 
 
 @app.get("/")
 async def root():
-    return {"message": "Simple API with Docker"}
-
-@app.post("/processrandom/")
-async def process(process_config: ProcessConfig):
     '''
-    perform a random process according to a configuration file and an environment variable
+    root path with site name
     '''
 
     # access environment variable values
-    site_name = environ['SITENAME']
+    return {"message": "Simple API with Docker to site: {}".format(site_name)}
 
-    # read configuration file
-    config_file_path = dir_config + process_config.file_name
 
+@app.post("/processrandom/")
+async def process(process_file: ProcessFile):
+    '''
+    read a file from the "data/" folder and perform a random process according to a configuration file and environment variable
+    '''
+
+    # read file from data folder
+    process_file_name = process_file.file_name
+    process_file_name_path = "data/" + process_file_name
     try:
-        with open(config_file_path, 'r', encoding='latin-1') as f:
-            config = load(f)
-        config_file_loaded = True
+        with open(process_file_name_path, 'r', encoding='latin-1') as f:
+            data = load(f)
 
     except FileNotFoundError:
-        config = "configuration file not found"
-        config_file_loaded = False
+        data = "data file not found"
 
     # generate a random process result
     result = choice(["ok", "fail"])
 
     return {"name": site_name,
-            "config_file_loaded": config_file_loaded,
             "config": config,
+            "data": data,
             "result": result,
             }
